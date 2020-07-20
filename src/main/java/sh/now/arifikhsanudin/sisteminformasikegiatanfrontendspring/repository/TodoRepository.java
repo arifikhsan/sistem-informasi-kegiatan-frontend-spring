61339c6ea606c5e6de370971b7b8439e4df3d682
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-07-20 21:29:42 +0700

    view

--- src/main/java/sh/now/arifikhsanudin/sisteminformasikegiatanfrontendspring/repository/TodoRepository.java
+++ src/main/java/sh/now/arifikhsanudin/sisteminformasikegiatanfrontendspring/repository/TodoRepository.java
@@ -1,14 +1,20 @@
 package sh.now.arifikhsanudin.sisteminformasikegiatanfrontendspring.repository;
 
+import org.springframework.web.reactive.function.client.WebClient;
 import sh.now.arifikhsanudin.sisteminformasikegiatanfrontendspring.model.TodoModel;
 
 import java.util.ArrayList;
 
 public class TodoRepository {
 
+    public WebClient client = WebClient.create("http://localhost:8080");
+
     public Iterable<TodoModel> all() {
+        WebClient.RequestHeadersSpec request = client.get().uri("/todo/all");
+        TodoModel res = request.retrieve().bodyToMono(TodoModel.class).block();
+        System.out.println(res);
         ArrayList<TodoModel> todos = new ArrayList<TodoModel>();
-        todos.add(new TodoModel("aa", false));
+        todos.add(new TodoModel((long) 1, "aa", false));
         return todos;
     }
 }
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,5 @@
+const todoName = document.getElementById("name");
+
+function apa() {
+    alert(todoName.value);
+}
